25e6c70be55ad276b3acf494548e06b600de4e6c
angelachan64 <angelachan64@gmail.com>
2015-01-06 08:40:57 -0500

    more updates

--- CAI/CAI.java
+++ CAI/CAI.java
@@ -7,18 +7,24 @@ import java.io.*;
 
 public class CAI extends JFrame{
     private Container window;
-    private JPanel title;
+    private JPanel title,black;
     private JButton newgame,loadgame;
     private BufferedImage titlepic;
-    private JLabel titulo;
+    private JLabel titulo,spacer;
     public CAI(){
         setTitle("Cards Against Insanity");
         setSize(800,800);
         setLocation(400,50);
+	getContentPane().setBackground(Color.BLACK);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         window = getContentPane();
 	window.setLayout(new FlowLayout());
+	
+	title = new JPanel();
+	title.setPreferredSize(new Dimension(600,55));
+        title.setOpaque(false);
+	window.add(title);
 
 	try{
 	titlepic = ImageIO.read(new File("title.png"));
@@ -32,9 +38,16 @@ public class CAI extends JFrame{
 	title.setBorder(BorderFactory.createLineBorder(Color.red,2));
 	window.add(title);*/
 
+	black = new JPanel();
+	black.setPreferredSize(new Dimension(600,55));
+	black.setOpaque(false);
+	window.add(black);
+
 	newgame = new JButton("New Game");
+	newgame.setPreferredSize(new Dimension(300,40));
 	window.add(newgame);
 	loadgame = new JButton("Load Game");
+	loadgame.setPreferredSize(new Dimension(300,40));
 	window.add(loadgame);
     }
 }
