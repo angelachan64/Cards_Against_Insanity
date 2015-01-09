cede897861f7f25c9b609aa4eaf1e7597a5b5ebc
angelachan64 <angelachan64@gmail.com>
2015-01-09 08:42:22 -0500

    things

--- CAI/game.java
+++ CAI/game.java
@@ -6,9 +6,10 @@ import java.util.*;
 import java.awt.*;
 import java.io.*;
 
-public class game extends JFrame implements KeyListener{
+public class game extends JFrame{
     private Container jonjo;
     private JPanel canvas;
+    private BufferedImage background,character;
 
     public game(){
 	setTitle("Cards Against Insanity");
@@ -20,16 +21,24 @@ public class game extends JFrame implements KeyListener{
 	jonjo.setLayout(new FlowLayout());
 	setVisible(true);
 
-	canvas = new Canvas();
-	canvas.setPreferredSize(new Dimension(600,600));
-        canvas.setBackground(Color.BLUE);
+	//canvas.setPreferredSize(new Dimension(600,600));
+        //canvas.setBackground(Color.BLUE);
+	try{
+	    image = ImageIO.read(new File("background.jpg"));
+	} catch(IOException e){
+	}
 	jonjo.add(canvas);
     }
 
-    private class Canvas extends JPanel{
+    /*private class Canvas extends JPanel{
 	public void paintComponent(Graphics g){
 	    super.paintComponent(g);
 	    g.setColor(Color.blue);
 	}
+    }*/
+
+    public static void main(String[] args){
+	game g = new game();
+	g.setVisible(true);
     }
 }
