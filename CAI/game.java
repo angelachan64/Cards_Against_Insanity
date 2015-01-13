8ce9ec240ae67ef26eb09d9ebcf1d4a12bada3f9
angelachan64 <angelachan64@gmail.com>
2015-01-13 08:42:11 -0500

    things

--- CAI/game.java
+++ CAI/game.java
@@ -6,12 +6,13 @@ import java.util.*;
 import java.awt.*;
 import java.io.*;
 
-public class game extends JFrame{
+public class game extends JFrame implements ActionListener{
     private Container jonjo;
     private JPanel canvas;
     private BufferedImage background,character;
-    private JTextArea name,entername;
-    private JButton Submit;
+    private JTextArea text,entername;
+    private JButton Submit,confirm,deny;
+    private String player;
 
     public game(){
 	setTitle("Cards Against Insanity");
@@ -34,27 +35,55 @@ public class game extends JFrame{
 	}
 	canvas = new Canvas();
 	jonjo.add(canvas);
-	name = new JTextArea();
-	name.append("What is your name?");
-	name.setColumns(20);
-	name.setRows(1);
-	name.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-	name.setEditable(false);
-	canvas.add(name);
+	text = new JTextArea();
+	text.setText("What is your name?");
+	text.setColumns(20);
+	text.setRows(1);
+	text.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	text.setEditable(false);
+	canvas.add(text);
+
 	entername = new JTextArea();
 	entername.setColumns(20);
-	name.setRows(1);
-	name.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-	entername.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	entername.setLineWrap(true);
+	text.setRows(1);
+	text.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	//entername.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 	canvas.add(entername);
 
 	Submit = new JButton("Submit");
+	Submit.addActionListener(this);
 	canvas.add(Submit);
+	confirm = new JButton("Yes");
+	confirm.addActionListener(this);
+	confirm.setVisible(false);
+	deny = new JButton("No");
+	deny.addActionListener(this);
+	deny.setVisible(false);
+	canvas.add(deny);
+	canvas.add(confirm);
     }
 
     public void actionPerformed(ActionEvent e) {
 	if (e.getSource() == Submit) {
-
+	    player = entername.getText();
+	    if (player.length() > 20){
+		text.setText("Your name is too long");
+		entername.setText("");
+	    } else{
+		text.setText("Are you sure you wish to be called " + player + "?");
+		text.setColumns(60);
+		entername.setVisible(false);
+		Submit.setVisible(false);
+		confirm.setVisible(true);
+		deny.setVisible(true);
+	    }
+	} else if(e.getSource() == deny){
+	    entername.setVisible(true);
+	    Submit.setVisible(true);
+	    confirm.setVisible(false);
+	    deny.setVisible(false);
+	    text.setText("What would you like to be called?");
 	}
     }
 
