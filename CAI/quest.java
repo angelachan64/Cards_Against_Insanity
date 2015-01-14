0f4d4879f91c855d8beb26f89d2ff4fb2250b8a8
Victor Tan <victor@gmail.com>
2015-01-14 08:43:06 -0500

    .

---
+++ CAI/quest.java
@@ -0,0 +1,19 @@
+import java.awt.image.*;
+import java.awt.event.*;
+import javax.imageio.*;
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+import java.io.*;
+
+public class quest extends JFrame implements MouseListener {
+    private Container battle;
+    private JPanel canvas;
+    private JButton start;
+
+    private quest() {
+	setTitle("Time to d-d-d-d-duel!");
+	setSize(800,800);
+	setLocation(400,50);
+    }
+}
